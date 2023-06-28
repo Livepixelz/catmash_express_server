259b30f73fe9a2b9e9bd48fd234ef2349d8a0517
solengha <solengha@vodfactory.com>
2023-06-28 19:28:58 +0200

    config

---
+++ app.js
@@ -0,0 +1,69 @@
+import express from "express"
+import { createClient } from "@supabase/supabase-js"
+import morgan from "morgan"
+import cors from "cors"
+import bodyParser from "body-parser"
+import { config } from "dotenv"
+
+config()
+
+const app = express()
+
+app.use(cors())
+app.use(morgan("combined"))
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+const supabase = createClient(
+  process.env.SUPABASE_API_URL,
+  process.env.SUPABASE_API_KEY,
+  {
+    auth: {
+      autoRefreshToken: true,
+      persistSession: false,
+      detectSessionInUrl: true
+    }
+  }
+)
+
+app.get("/players", async (req, res) => {
+  let request = supabase
+    .from("players")
+    .select()
+    .order("score", { ascending: false })
+  if (req.query._sort) {
+    request = request.order(req.query._sort, {
+      ascending: req.query?._order === "asc"
+    })
+  }
+  const { data, error } = await request
+  res.send(data)
+})
+
+app.patch("/players/:id", async (req, res) => {
+  const { error } = await supabase
+    .from("players")
+    .update({
+      firstname: req.body.firstname,
+      url: req.body.url,
+      score: req.body.score
+    })
+    .eq("id", req.params.id)
+  if (error) {
+    res.send(error)
+  }
+  res.send("updated!!")
+})
+
+app.get("/", (req, res) => {
+  res.send("Catmash API")
+})
+
+app.get("*", (req, res) => {
+  res.send("Catmash API")
+})
+
+app.listen(3000, () => {
+  console.log(`> Ready on http://localhost:3000`)
+})
